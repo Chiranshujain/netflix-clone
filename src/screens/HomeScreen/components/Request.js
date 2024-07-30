9be177c8b296e90718936ff01f6d435140a417e3
Chiranshujain <122280517+Chiranshujain@users.noreply.github.com>
2024-07-30 18:58:41 +0530

    Your descriptive commit message

--- src/screens/HomeScreen/components/Request.js
+++ src/screens/HomeScreen/components/Request.js
@@ -1,12 +1,12 @@
 const API_KEY = "ae96c2c5322697f3e275e82d6615166b";
 
 const requests = {
-    fetchTrendings: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTrendings: `/trending/all/week?api_key=${API_KEY}&language=en-UK`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-UK`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
