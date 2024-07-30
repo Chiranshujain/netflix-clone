f07f0d82fbf7a9ce21b36409d7dab2562fd7b93f
Chiranshujain <122280517+Chiranshujain@users.noreply.github.com>
2024-07-30 21:44:09 +0530

    Update Request.js

--- src/screens/HomeScreen/components/Request.js
+++ src/screens/HomeScreen/components/Request.js
@@ -1,14 +1,17 @@
 const API_KEY = "ae96c2c5322697f3e275e82d6615166b";
 
 const requests = {
-    fetchTrendings: `/trending/all/week?api_key=${API_KEY}&language=en-UK`,
+   fetchTrendings: `/trending/all/week?api_key=${API_KEY}&language=en-GB`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-UK`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-GB`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=36`,
+    fetchUpcomingMoviesIndia: `/movie/upcoming?api_key=${API_KEY}&language=en-IN&region=IN`,
+    fetchTvShows : `/tv/top_rated?api_key=${API_KEY}&language=en-IN`,
+    fetchTopHindiMovies :`/discover/movie?api_key=${API_KEY}&with_original_language=hi&sort_by=vote_average.desc&vote_count.gte=50`,
 }
 
 
